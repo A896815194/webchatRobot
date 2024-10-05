dfc03adc2034d7e7206cd5be1d5c72023d7d3827
A896815194 <896815194@qq.com>
2024-10-06 02:55:26 +0800

    code clean

--- src/main/java/com/web/webchat/api/HtmlController.java
+++ src/main/java/com/web/webchat/api/HtmlController.java
@@ -18,7 +18,7 @@ import java.util.stream.Collectors;
 @Slf4j
 public class HtmlController {
 
-    @GetMapping("/index")
+    @GetMapping("")
     public String index() {
 
         return "welcome";
